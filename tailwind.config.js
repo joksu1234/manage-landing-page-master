d63e49b482ad73914976c95ad5c6086718d24551
Joksu <joakim.kauranen@gmail.com>
2024-01-17 14:45:57 +0200

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'bright-red': 'hsl(12, 88%, 59%)',
+      'dark-blue': 'hsl(228, 39%, 23%)',
+      'dark-grayish-blue': 'hsl(227, 12%, 61%)',
+      'very-dark-blue': 'hsl(233, 12%, 13%)',
+      'very-pale-red': 'hsl(13, 100%, 96%)',
+      'very-light-gray': 'hsl(0, 0%, 98%)',
+    }
+  },
+  plugins: [],
+}
+
