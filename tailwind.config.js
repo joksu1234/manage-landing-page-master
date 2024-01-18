f2c4f089a2de854ded6cd11fec7708b7fe78b62a
Joksu <joakim.kauranen@gmail.com>
2024-01-18 15:07:07 +0200

    Completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,25 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     colors: {
+      'white': 'hsl(0, 0%, 100%)',
       'bright-red': 'hsl(12, 88%, 59%)',
+      'bright-red-light': 'hsl(12, 88%, 69%)',
+      'bright-red-suplight': 'hsl(12, 88%, 95%)',
       'dark-blue': 'hsl(228, 39%, 23%)',
       'dark-grayish-blue': 'hsl(227, 12%, 61%)',
       'very-dark-blue': 'hsl(233, 12%, 13%)',
       'very-pale-red': 'hsl(13, 100%, 96%)',
       'very-light-gray': 'hsl(0, 0%, 98%)',
+    },
+    fontFamily: {
+      'sans-serif': 'Be Vietnam Pro',
     }
   },
   plugins: [],
